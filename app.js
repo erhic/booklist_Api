49ac64e32f7a92410350b30c719342494f3fe9eb
erhic <ericngugi24@gmail.com>
2023-04-26 00:51:42 +0300

    added a class with a construcor to create different instances of the object

---
+++ app.js
@@ -0,0 +1,19 @@
+// Book Class >>> a class to represent a book and its propeties
+class Book{
+    constructor(title,author,isbn){
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
+    }
+}
+
+
+//UI Class >>>  a class to make up the  DOM , handlee UI tasks 
+
+// Store Class >>> a class that will handle storage
+
+// Event >>> Dispay the books
+
+// Event >>> Add a book
+
+// Event >>> Remove a book
