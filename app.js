c5ccefbe581e34de57febb4e554a581a4e661bcd
erhic <ericngugi24@gmail.com>
2023-04-26 01:32:48 +0300

    queried the DOM in JS , got rid of the parent element ,stored in a variable , created  a child element in JS  , then appended the child to to parent, child has static values the will be dispayed for every instance in our object array the was restored in a single a variabl for looping thorough it

--- app.js
+++ app.js
@@ -11,17 +11,28 @@ class Book{
 //UI Class >>>  a class to make up the  DOM , handlee UI tasks 
 class UI{
    static displayBooks(){
- const StoredBooks=[
-    {title:'Atomic Habits',
-    author:'JG Mitch',
-    isbn:'4556'},
-    
-    {title:'River Bank',
-    author:'Antol',
-    isbn:'24453'}, 
- ];
+        const StoredBooks=[
+            {title:'Atomic Habits',
+            author:'JG Mitch',
+            isbn:'4556'},
+            
+            {title:'River Bank',
+            author:'Antol',
+            isbn:'24453'}, 
+        ];
    
- const book = StoredBooks;
+        const books = StoredBooks;
+        books.forEach((book)=>UI.addBookToList(book))
+   }
+   static addBookToList(book){
+    const list= document.querySelector('#book-list')
+    const row= document.createElement('tr')
+    row.innerHTML=
+   ` <td> ${book.title}</td>
+    <td> ${book.author}</td>
+    <td> ${book.isbn}</td>
+    <td> <a href=# class=btn btn-danger btn-sm delete>X </a></td>
+    `;
 
    }
 }
