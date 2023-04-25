15513552867cb3d9bc8e0dca14933f74b3b4237f
erhic <ericngugi24@gmail.com>
2023-04-26 01:47:21 +0300

    added the inner HTML context values that are be displayed with reference to variable data of the array, added a event listener  event , on loading the document , UI Class displyBooks funtion to be executed

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ class UI{
     const list= document.querySelector('#book-list')
     const row= document.createElement('tr')
     row.innerHTML=
-   ` <td> ${book.title}</td>
+   `<td> ${book.title}</td>
     <td> ${book.author}</td>
     <td> ${book.isbn}</td>
-    <td> <a href=# class=btn btn-danger btn-sm delete>X </a></td>
+    <td> <a href=# class="btn btn-danger btn-sm delete">X </a></td>
     `;
-
+    list.appendChild(row)
    }
 }
 
@@ -42,7 +42,7 @@ class UI{
 // Store Class >>> a class that will handle storage
 
 // Event >>> Dispay the books
-
+document.addEventListener('DOMContentLoaded',UI.displayBooks)
 // Event >>> Add a book
 
 // Event >>> Remove a book
