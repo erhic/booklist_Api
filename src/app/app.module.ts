eefa378684691b95170c5d8c3c2756ab99adcfb2
erhic <ericngugi24@gmail.com>
2023-08-22 19:35:20 +0300

    ✨ intial commit ,set up components and default route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { AccountComponent } from './account/account.component';
+
+const  routes=[
+  {path:'/',component:AccountComponent},
+  {path:'',component:AccountComponent},
+  {path:'',component:AccountComponent},
+  {path:'',component:AccountComponent},
+  {path:'',component:AccountComponent},
+  {path:'',component:AccountComponent},
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavComponent,
+    DepositComponent,
+    WithdrawComponent,
+    TransferComponent,
+    SidenavComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
